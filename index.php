f126c27c3403f126612a239cdc8a99baddc76a09
martin <martdevelopers254@gmail.com>
2020-07-29 15:23:44 +0300

    Added Sweet Alerts

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php
-
-require_once('partials/_head.php');
+      session_start();
+      include('config/config.php');
+      //login 
+      if(isset($_POST['login']))
+      {
+         $login_user_permission = $_POST['login_user_permission'];
+         $login_user_email = $_POST['login_user_email'];
+         $login_user_password = sha1(md5($_POST['login_user_password']));//double encrypt to increase security
+         $stmt=$mysqli->prepare("SELECT login_user_permission, login_user_email, login_user_password, login_id  FROM iBookStore_Login  WHERE login_user_permission =? AND login_user_email =? AND login_user_password =?");//sql to log in user
+         $stmt->bind_param('iss',  $login_user_permission, $login_user_email, $login_user_password);//bind fetched parameters
+         $stmt->execute();//execute bind 
+         $stmt -> bind_result($login_user_permission, $login_user_email, $login_user_password, $login_id);//bind result
+         $rs=$stmt->fetch();
+         $_SESSION['login_id'] = $login_id;
+         if($rs && $login_user_permission == '1')
+         {
+           //if its sucessfull
+           header("location:pages_admin_dashboard.php");
+         }
+         else
+         {
+           $err = "Incorrect Authentication Credentials ";
+         }
+      }
+    require_once('partials/_head.php');
 ?>
 <body class="form">
     <div class="form-container outer">
@@ -16,7 +39,7 @@ require_once('partials/_head.php');
                                 <div id="username-field" class="field-wrapper input">
                                     <label for="username">Email</label>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
-                                    <input id="username" name="login_user_email" type="text" class="form-control">
+                                    <input id="username"  name="login_user_email" type="email" required class="form-control">
                                 </div>
 
                                 <div id="password-field" class="field-wrapper input mb-2">
@@ -25,12 +48,13 @@ require_once('partials/_head.php');
                                         <a href="pages_admin_reset_pwd.php" class="forgot-pass-link">Forgot Password?</a>
                                     </div>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
-                                    <input id="password" name="login_user_password" type="password" class="form-control" placeholder="Password">
+                                    <input id="password" required name="login_user_password" type="password" class="form-control" placeholder="Password">
+                                    <input id="" name="login_user_permission" value="1" type="text" style="display:none" class="form-control" placeholder="Password">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
                                 </div>
                                 <div class="d-sm-flex justify-content-between">
                                     <div class="field-wrapper">
-                                        <button type="submit" name="login" class="btn btn-primary" value="">Log In</button>
+                                        <button type="submit" name="login" class="btn btn-primary mixin" value="">Log In</button>
                                     </div>
                                 </div>
 
--- partials/_scripts.php
+++ partials/_scripts.php
@@ -2,3 +2,6 @@
 <script src="bootstrap/js/popper.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="assets/js/authentication/form-2.js"></script>
+<script src="assets/js/scrollspyNav.js"></script>
+<script src="plugins/sweetalerts/sweetalert2.min.js"></script>
+<script src="plugins/sweetalerts/custom-sweetalert.js"></script>
